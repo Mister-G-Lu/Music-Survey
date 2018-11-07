8e3f44dcb5883747cd6d82af45b56f946a777cb1
jackj8 <44556325+jackj8@users.noreply.github.com>
2018-11-07 17:32:39 -0500

    Add files via upload

---
+++ AllEnum.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package project5;
+
+/**
+ * Enum holder class
+ * @author Jack Juechter
+ * @version 2018.11.7
+ */
+public class AllEnum {
+    //Enum for hobby choices
+    public enum hobbyEnum {READ, ART, SPORTS, MUSIC};
+    //Enum for major choices
+    public enum majorEnum {CS, OTHERENG, MATHCMDA, OTHER};
+    //Enum for region choices
+    public enum regionEnum {NEUS, SEUS, RESTUS, OUTUS};
+    //Enum for song choices
+    public enum HLBEnum {HEARD, LIKE, BLANK};
+}
