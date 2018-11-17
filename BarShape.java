270c1b70dbb3c9fc4950965bb3af092a97ccc8a4
Goodwin Lu <gugigor@live.com>
2018-11-17 11:34:43 -0500

    Add files via upload

---
+++ BarShape.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package project5;
+
+import java.awt.Color;
+import java.util.Random;
+import CS2114.Shape;
+
+/**
+ * @author Goodwin Lu
+ * @version 11/11/2018
+ *          the heard and liked percent for each song, calculated in GUI, is
+ *          stored here, then used to create the bars
+ */
+public class BarShape extends Shape {
+
+    private static Random r = new Random();
+    private int heardPercent;
+    private int likedPercent;
+
+
+    /**
+     * constructor
+     * 
+     * @param s
+     *            the song
+     * @param h
+     *            height
+     * @param l
+     *            length
+     * 
+     */
+    public BarShape(int h, int l) {
+        super(-h, l, 5, new Color(r.nextInt(255)));
+        heardPercent = h;
+        likedPercent = l;
+    }
+
+
+    /**
+     * get heard
+     * 
+     * @return
+     *         the heard percent
+     */
+    public int getHeard() {
+        return heardPercent;
+    }
+
+
+    /**
+     * get liked
+     * 
+     * @return
+     *         liked percent
+     */
+    public int getLiked() {
+        return likedPercent;
+    }
+}
