af7852ac4573f8b3652532da3f781191af8b4611
Goodwin Lu <gugigor@live.com>
2018-11-25 13:06:09 -0500

    Update SortSongs.java

--- SortSongs.java
+++ SortSongs.java
@@ -15,27 +15,53 @@ public class SortSongs {
     // genre, and LL<T> people in that order
     private SortedList<T[]> allSongs;
 
-
+    
+    /**
+    * default constructor.
+    * @param temp
+    * the linked list we use.
+    */
     public SortSongs(LinkedList<T[]> temp) {
         allSongs = temp;
     }
 
-
+    /**
+    * get the song.
+    * @return
+    * The array "allsongs"
+    */
+    public SortedList<T[]> getSongs(){
+        return allSongs;
+    }
+    /**
+    * sort artists.
+    * @return sorted list artist wise
+    */
     public LinkedList<T> sortArtists() {
         // case does NOT matter in this method
         allSongs.insertSortString(0);
     }
 
-
+ /**
+    * sort title.
+    * @return sorted list title wise
+    */
     public LinkedList<T> sortTitles() {
         allSongs.insertSortString(1);
     }
 
-
+ /**
+    * sort dates.
+    * @return sorted list date wise
+    */
     public LinkedList<T> sortAscendingDate() {
         allSongs.insertSortString(2);
     }
     
+     /**
+    * sort genres.
+    * @return sorted list genre wise
+    */
     public LinkedList<T> sortGenres() {
         // Case DOES matter in this sorting method
         allSongs.insertSortString(3);
